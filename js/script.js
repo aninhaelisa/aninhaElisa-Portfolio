579a08d0dd03145f7bf56c19513403d44453c181
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-30 17:25:50 -0300

    Implementação do visual principal

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.body.style.overflow = 'hidden';
+
+function iniciarDataEHora() {
+
+    now = new Date();
+    dayName = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado");
+    month = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+    dataAtual = dayName[now.getDay()] + ", " + now.getDate() + " " + month[now.getMonth()] + " " + now.getFullYear();
+    var data = document.querySelector(".data");
+    data.textContent = dataAtual;
+    function hora() {
+        horaAtual = new Date();
+        h = horaAtual.getHours();
+        m = horaAtual.getMinutes();
+        if (m < 10) {
+            m = "0" + m;
+        }
+        if (h < 10) {
+            h = "0" + h;
+        }
+        document.querySelector(".hora").innerHTML = h + ":" + m;
+        setTimeout(hora, 500);
+    }
+    hora();
+}
+
+window.onload = iniciarDataEHora;
