b81d476c22dd08f74107e3bd65ca23d38d01a726
aninhaelisa <elisaana.2019.2006@gmail.com>
2026-08-31 15:05:03 -0300

    Finalizando mascote

--- js/mascote.js
+++ js/mascote.js
@@ -8,7 +8,7 @@ const expressoes = [
     {
         imagem: "../src/mascote/falando.gif",
         falas: [
-            "Oi :D",
+            "System.out.print(''HELLO WORLD'');",
             "Bem-vindo ao meu cantinho!",
             "Você chegou até aqui :)",
             "Hmmmm...",
@@ -60,7 +60,7 @@ const expressoes = [
         ]
     },
     {
-        imagem: "../src/mascote/morto.gif", 
+        imagem: "../src/mascote/morto.gif",
         falas: [
             "404: mascote não encontrado...",
             "git push --force",
@@ -69,36 +69,123 @@ const expressoes = [
         ]
     },
     {
-        imagem: "../src/mascote/dormi.gif",
+        imagem: "../src/mascote/empolgado.gif",
         falas: [
             "O BUG SUMIU!!! ヽ(>▽<)ﾉ",
             "NÃO ACREDITO QUE FUNCIONOU!!",
             "MAIS UM BUG DERROTADO!!! >:D",
             "ACHEI O BUG!"
         ]
+    },
+    {
+        imagem: "../src/mascote/escrevendo.gif",
+        falas: [
+            "Anotando uma ideia... ✎",
+            "Escrevendo o código... no papel",
+            "Documentação é importante! :D",
+            "Escrevendo uma solução brilhante... talvez"
+        ]
+    },
+    {
+        imagem: "../src/mascote/procurar.gif",
+        falas: [
+            "Onde você está o bug?",
+            "Analisando o código...",
+            "Eu vou encontrar esse bug",
+            "Onde foi parar esse ponto e vírgula?"
+        ]
+    },
+    {
+        imagem: "../src/mascote/tchau.gif",
+        falas: [
+            "Hello World! ",
+            "Um commit de boas-vindas!",
+            "Aceno.exe iniciado com sucesso!",
+            "pspsps..."
+        ]
+    },
+    {
+        imagem: "../src/mascote/sono.gif",
+        falas: [
+            "Zzzzz...",
+            "Meu cérebro deu timeout",
+            "Sleep();",
+            "while(true) { dormir(); }"
+        ]
+    },
+    {
+        imagem: "../src/mascote/cantando.gif",
+        falas: [
+            "Eu inventei essa música agora",
+            "Não interrompa o show",
+            "♪ Não tem bug quando eu canto ♪",
+            "♪ Meu código tem ritmo ♪"
+        ]
+    },
+    {
+        imagem: "../src/mascote/dormi.gif",
+        falas: [
+            "Erro 503: energia temporariamente indisponível",
+            "RAM: ocupada com sono",
+            "Energia: 1%",
+            "Sistema operacional: boa noite"
+        ]
     }
+
 ];
 
+let filaFalas = [];
 
-function escolherAleatorio(lista) {
 
-    const indice = Math.floor(
-        Math.random() * lista.length
-    );
+function criarFilaFalas() {
 
-    return lista[indice];
+    filaFalas = [];
+
+    expressoes.forEach(expressao => {
+
+        expressao.falas.forEach(fala => {
+
+            filaFalas.push({
+                imagem: expressao.imagem,
+                fala: fala
+            });
+
+        });
+
+    });
+
+
+    // Embaralha a fila
+    for (let i = filaFalas.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [filaFalas[i], filaFalas[j]] =
+            [filaFalas[j], filaFalas[i]];
+
+    }
 }
 
 
+criarFilaFalas();
+
 function mostrarFala() {
 
-    const expressao = escolherAleatorio(expressoes);
-    const fala = escolherAleatorio(expressao.falas);
+    if (filaFalas.length === 0) {
 
-    mascoteImagem.src = expressao.imagem;
-    mascoteFala.textContent = fala;
+        criarFilaFalas();
+
+    }
+
+    const proxima = filaFalas.pop();
+
+
+    mascoteImagem.src = proxima.imagem;
+
+    mascoteFala.textContent = proxima.fala;
 
     mascoteBalao.classList.add("visivel");
+
 
     setTimeout(() => {
 
@@ -107,23 +194,18 @@ function mostrarFala() {
     }, 5000);
 }
 
-
-/* Primeira fala */
-
 setTimeout(() => {
 
     mostrarFala();
 
-}, 2500);
-
-
-/* Fala novamente em intervalos aleatórios */
+}, 4000);
 
 function programarProximaFala() {
 
     const tempo = Math.floor(
-        Math.random() * (15000 - 7000) + 7000
+        Math.random() * (20000 - 10000) + 10000
     );
+
 
     setTimeout(() => {
 
